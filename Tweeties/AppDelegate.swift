25d88483d9d9dab0d10e785cf5bb3464845a0902
Kostya Likhachev <arrow.koss@gmail.com>
2019-04-30 10:41:20 +0300

    - added Twitter Kit

--- Tweeties/AppDelegate.swift
+++ Tweeties/AppDelegate.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-
-@UIApplicationMain
+//import Twitter
+//@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
@@ -16,11 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        TWTRTwitter.sharedInstance().start(withConsumerKey:"2oytvfBOLN1rUi5XHHHzNPvYI", consumerSecret:"dxSwcCXl8KT15tiDVJbcj4fInxQDVXnqUDfV5U0gJC8vocOHWY")
+
         UITabBar.appearance().tintColor = UIColor(red: 136/255.0, green: 153/255.0, blue: 166/255.0, alpha: 1.0)
         return true
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return TWTRTwitter.sharedInstance().application(app, open: url, options: options)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ Tweeties/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  Tweeties
+//
+//  Created by mac on 4/30/19.
+//  Copyright © 2019 Developers Academy. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+#import "TwitterKit/TWTRKit.h"
+
+#endif /* BridgingHeader_h */
